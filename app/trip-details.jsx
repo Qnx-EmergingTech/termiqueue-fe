44084fc5a111e7e11e207b272e3c7f34467dcea2
QNXCjLabao <136563013+QNXCjLabao@users.noreply.github.com>
2026-05-22 10:16:43 +0800

    Revert "FIX: changed bus to shuttle"

--- app/trip-details.jsx
+++ app/trip-details.jsx
@@ -105,7 +105,7 @@ useEffect(() => {
 
         <View style={styles.details}>
           <Text style={styles.data}>
-            Shuttle Number: {trip.bus_number}
+            Bus Number: {trip.bus_number}
           </Text>
           <Text style={styles.data}>
             Origin: {trip.origin}

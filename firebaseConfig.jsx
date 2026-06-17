3caba1af3440fd67c20148497976ca5693357913
Danah Diokno <danah.edd_diokno@questronix.com.ph>
2026-06-17 10:59:39 +0800

    FIX: changed firebase config for new firebase acct and env cleanup

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -9,12 +9,13 @@ import { getFirestore } from "firebase/firestore";
 import { Platform } from "react-native";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5ai21uU_Ln_2FlSxXpB3Wmn4tzLGmzx0",
-  authDomain: "qnext-bc282.firebaseapp.com",
-  projectId: "qnext-bc282",
-  storageBucket: "qnext-bc282.firebasestorage.app",
-  messagingSenderId: "655635157366",
-  appId: "1:655635157366:web:041042eac22a7153c0aa57"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
